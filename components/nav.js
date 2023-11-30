42acd1c03aa778a04e616cddcac29352b1be2c57
jreider2 <jreider@luc.edu>
2023-11-30 11:10:16 -0800

    add nav.js component

---
+++ components/nav.js
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+export default function Nav() {
+  return (
+    <nav>
+      <Link href="/work">Work</Link>
+      <Link href="/about">About</Link>
+      <Link href="/contact">Contact</Link>
+    </nav>
+  );
+}
