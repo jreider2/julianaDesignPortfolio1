36640c2555457e4ca9928b15e437ace68acb7647
jreider2 <jreider@luc.edu>
2023-12-04 15:44:54 -0800

    some styling for the cards on the work page. start using flex box to creat the correct placement.

--- components/teasercard.js
+++ components/teasercard.js
@@ -4,10 +4,14 @@ import styles from "./teasercard.module.css";
 export default function TeaserCard({ title, description, image }) {
   return (
     <div className={styles.card}>
-      <h3>{title}</h3>
-      <p>{description}</p>
-      <img src="/images/arrow.svg" alt="arrow" />
-      <img src={image} alt={title} className={styles.item1}/>
+      <div className={styles.top_container}>
+        <div className={styles.text_content}>
+          <h3>{title}</h3>
+          <p>{description}</p>
+        </div>
+        <img src="/images/arrow.svg" alt="arrow" />
+      </div>
+      <img src={image} alt={title} className={styles.image} />
     </div>
   );
 }
