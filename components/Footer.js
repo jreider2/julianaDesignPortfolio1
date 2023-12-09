646a971e70aa61952c5fc7f050a9b6b3add00718
jreider2 <jreider@luc.edu>
2023-12-08 18:35:19 -0800

    mess with the footer again.

--- components/Footer.js
+++ components/Footer.js
@@ -11,30 +11,37 @@ export default function Footer() {
         <p>
           {/* Juliana Reider 
           <span className={styles.separator}>•</span>  */}
-          Made with ♥
-          {/* <span className={styles.separator}>•</span> 
-          {new Date().getFullYear()}  */}
+          portfolio coded with love
+          {/* <span className={styles.separator}></span> 
+          ♥<span className={styles.separator}></span>  */}
+          {/* <span className={styles.separator}>•</span>  */}
+          {/* {new Date().getFullYear()}  */}
+          <span className={styles.separator}></span> 
+            {'  '}
+          <a href="https://github.com/jreider2/nextjs-blog" target="_blank">
+            <Image src={GitHubIcon} alt="github" className={styles.GitHubIcon} width={20} height={20} />
+          </a>
         </p>
         {/* <div className={styles.linksContainer}>Made with &lt;3</div> */}
         <p>
-            Email me at{' '}
+            email {' '}
             <a href="mailto:jreider@luc.edu" className={styles.email}>
               jreider@luc.edu
             </a>
           </p>
           <div className={styles.linksContainer}>
           <Link href="/" className={styles.homeLink}>
-            Home
+            home
           </Link>
           <a href="YOUR_RESUME_LINK" target="_blank" className={styles.resumeLink}>
-            Resume
+            resume
           </a>
           <a href="https://www.linkedin.com/in/juliana-reider/" target="_blank">
             <Image src={LinkedInIcon} alt="linkedIn" className={styles.LinkedInIcon} width={20} height={20} />
           </a>
-          <a href="https://github.com/jreider2/nextjs-blog" target="_blank">
+          {/* <a href="https://github.com/jreider2/nextjs-blog" target="_blank">
             <Image src={GitHubIcon} alt="github" className={styles.GitHubIcon} width={20} height={20} />
-          </a>
+          </a> */}
         </div>
       </div>
     </div>
