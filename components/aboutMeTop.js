70ffa61a1daa76c5f5bf4be04a12c1565cea09f5
jreider2 <jreider@luc.edu>
2023-12-12 13:42:44 -0800

    add more content to about me page. continue stylijng. also flex boxing.

--- components/aboutMeTop.js
+++ components/aboutMeTop.js
@@ -6,12 +6,12 @@ import profileImage from '/public/images/me.png';
 
 export default function AboutMeTop({ children, home }) {
     return (
-        <div className ={styles.fullContent}>
-        <div className={styles.titleContainer}>
-            <h1 className={styles.title}>about me</h1>
+        <div className={styles.fullContent}>
+            <div className={styles.titleContainer}>
+                <h1 className={styles.title}>about me</h1>
 
-            <h2 className={styles.subtitle}>I’m Juliana, a software engineer turned product designer. </h2>
-        </div>
+                <h2 className={styles.subtitle}>I’m Juliana, a software engineer turned product designer. </h2>
+            </div>
             <div className={styles.flexContainer1}>
                 {/* Flexbox container 1 */}
                 <div className={styles.imageContainer}>
@@ -28,7 +28,8 @@ export default function AboutMeTop({ children, home }) {
                         where I found a deeper satisfaction in blending those skills with my visual
                         problem-solving abilities and my aesthetic sense on a daily basis.</p>
                 </div>
-            </div><div className={styles.flexContainer2}>
+            </div>
+            <div className={styles.flexContainer2}>
                 {/* Flexbox container 2 */}
                 <p> <span className={styles.emphasis2}>My pursuit of learning </span>
                     how to best design useable products,
@@ -36,21 +37,53 @@ export default function AboutMeTop({ children, home }) {
                     has enabled me to view design through
                     <span className={styles.emphasis2}> a unique lens.</span>
                 </p>
-            </div><div className={styles.flexContainer3}>
+            </div>
+            <div className={styles.flexContainer1}>
                 {/* Flexbox container 3 */}
+                <div className={styles.f1Text1}>
+                    <h3 className={styles.midTitle}>Arts and Science</h3>
+                    <p>My BA in Philosophy and English, followed by a
+                        <span className={styles.emphasis}> Masters in Computer Science</span>
+                        , has equipped me with a unique blend of creative and analytical skills.
+                        This mix is perfect for product design, where understanding technical and
+                        business constraints is as important as presenting information in a user-friendly way.
+                    </p>
+                </div>
                 <div className={styles.imageContainer}>
                     <Image src={profileImage} alt="Image" layout="intrinsic" />
                 </div>
-                <div>
-                    <h3 className={styles.midTitle}>text part 1</h3>
-                    <p>text part 1
-                        <span className={styles.emphasis}> specail text</span>
-                        end of paragraph</p>
-                </div>
-            </div><div className={styles.flexContainer4}>
+            </div>
+            <div className={styles.flexContainer1}>
                 {/* Flexbox container 4 */}
-            </div><div className={styles.flexContainer5}>
+                <div className={styles.imageContainer}>
+                    <Image src={profileImage} alt="Image" layout="intrinsic" />
+                </div>
+                <div className={styles.f1Text1}>
+                    <h3 className={styles.midTitle}>Collaboration, Communication, and Mentorship</h3>
+                    <p>My consultancy background underlines the importance of teamwork, proactive communication, and adaptability.
+                        These skills, along with my experience as an ESL teacher in Japan and
+                        <span className={styles.emphasis}> mentoring young engineers</span>
+                        , have ingrained a strong sense of mentorship and collaboration in me, which I eagerly bring to the world of design.
+                    </p>
+                </div>
+            </div>
+            <p className={styles.callout}>
+                Let’s create something
+                <span className={styles.calloutEmphasis}> amazing together.</span>
+            </p>
+            <div className={styles.flexContainer5}>
                 {/* Flexbox container 5 */}
+                <div className={styles.imageContainer}>
+                    <Image src={profileImage} alt="Image" layout="intrinsic" />
+                </div>
+                <div className={styles.f1Text1}>
+                    <h3 className={styles.midTitle}>Collaboration, Communication, and Mentorship</h3>
+                    <p>My consultancy background underlines the importance of teamwork, proactive communication, and adaptability.
+                        These skills, along with my experience as an ESL teacher in Japan and
+                        <span className={styles.emphasis}> mentoring young engineers</span>
+                        , have ingrained a strong sense of mentorship and collaboration in me, which I eagerly bring to the world of design.
+                    </p>
+                </div>
             </div>
         </div>
     );
