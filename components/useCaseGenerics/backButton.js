ad2da20927db3bfedc835ece6162d593508afc25
jreider2 <jreider@luc.edu>
2024-02-28 11:27:16 -0800

    fix back button on hover.

--- components/useCaseGenerics/backButton.js
+++ components/useCaseGenerics/backButton.js
@@ -13,8 +13,8 @@ export default function BackButton ({ onClick }) {
     <div className={`${styles.buttonContainer}`}>
         <button className={styles.button} onClick={handleClick}>
             <svg xmlns="http://www.w3.org/2000/svg" width="27" height="24" viewBox="0 0 27 24" fill="none">
-                <path d="M18.7059 12H6" stroke="#D5DCF4" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                <path d="M12.3529 18L6 12L12.3529 6" stroke="#D5DCF4" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                <path d="M18.7059 12H6" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                <path d="M12.3529 18L6 12L12.3529 6" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
             </svg>
             <span className={styles.buttonText}>Back</span>
         </button>
